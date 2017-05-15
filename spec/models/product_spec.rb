cf307081a9e046655a7380aa2350368b57dac9ce
kevmor11 <morissette.kevin@gmail.com>
2017-05-15 13:46:10 -0700

    added validation checks

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,25 +12,25 @@ RSpec.describe Product, type: :model do
 
   it 'should not create a product with an invalid name' do
     @product.name = nil
-    @product.save
+    expect(@product.valid?).to be false
     expect(@product.errors.full_messages).to include("Name can't be blank")
   end
   
   it 'should not create a product with an invalid price' do
     @product.price_cents = nil
-    @product.save
+    expect(@product.valid?).to be false
     expect(@product.errors.full_messages).to include("Price can't be blank")
   end
 
   it 'should not create a product with an invalid quantity' do
     @product.quantity = nil
-    @product.save
+    expect(@product.valid?).to be false
     expect(@product.errors.full_messages).to include("Quantity can't be blank")
   end
 
   it 'should not create a product with an invalid category' do
     @product.category = nil
-    @product.save
+    expect(@product.valid?).to be false
     expect(@product.errors.full_messages).to include("Category can't be blank")
   end
     
