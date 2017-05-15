0777b1be7f682fb948230f505a7e99ac8ac07c2e
kevmor11 <morissette.kevin@gmail.com>
2017-05-15 13:15:49 -0700

    implemented product spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  before(:each) do
+    @category = Category.new(name: 'category')
+    @product = Product.new(name: "product", price_cents: 10, quantity: 2, category: @category)
+  end    
+    
+  it 'should check if the product is created' do
+    expect(@product).to be_valid
+  end
+
+  it 'should not create a product with an invalid name' do
+    @product.name = nil
+    @product.save
+    expect(@product.errors.full_messages).to include("Name can't be blank")
+  end
+  
+  it 'should not create a product with an invalid price' do
+    @product.price_cents = nil
+    @product.save
+    expect(@product.errors.full_messages).to include("Price can't be blank")
+  end
+
+  it 'should not create a product with an invalid quantity' do
+    @product.quantity = nil
+    @product.save
+    expect(@product.errors.full_messages).to include("Quantity can't be blank")
+  end
+
+  it 'should not create a product with an invalid category' do
+    @product.category = nil
+    @product.save
+    expect(@product.errors.full_messages).to include("Category can't be blank")
+  end
+    
+end
