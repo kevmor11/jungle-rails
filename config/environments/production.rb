26e98c5d7163eb8925f74b74eb5cdecf79700cf2
kevmor11 <morissette.kevin@gmail.com>
2017-05-15 20:03:28 -0700

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.perform_deliveries = false
-  p ENV
   config.secret_token = ENV["SECRET_TOKEN"]
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
