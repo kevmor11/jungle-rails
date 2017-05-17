40439668cf0d1167375c37080f6988c12240f267
kevmor11 <morissette.kevin@gmail.com>
2017-05-16 18:31:32 -0700

    implemented order quantity adjustment feature and testing

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,12 +7,24 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  after_save :update_quantity
+
   def total_order_price
     line_items.sum(:total_price_cents)
   end
 
   def cart_is_empty
     if cart.size > 0
+    end
+  end
+
+  private
+
+  def update_quantity
+    self.line_items.all.each do |item|
+      product = Product.find(item.product_id)
+      product.quantity = product.quantity - item.quantity
+      product.save!
     end
   end
 
